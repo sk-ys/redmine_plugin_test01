099e8b8fbb5fdeaa252beff63d96e8b5138d9cae
sk-ys <963792+sk-ys@users.noreply.github.com>
2019-12-29 19:21:39 +0900

    update print_tree.bat, tree.txt

--- print_tree.bat
+++ print_tree.bat
@@ -1,2 +1,7 @@
-if exist tree.txt del tree.txt
-for /f "usebackq skip=3 tokens=* delims=" %%i in (`tree /a`) do @echo %%i >> tree.txt
+@echo off
+:: get parent directory name and initialize tree.txt
+for %%i in (%cd%) do set parent_directory=%%~ni
+echo %parent_directory% > tree.txt
+
+:: make tree
+for /f "usebackq skip=3 tokens=* delims=" %%i in (`tree /a %1`) do @echo %%i >> tree.txt
