6ed7f379e92d52c44ad10183f8aa7963ef723a8b
sk-ys <963792+sk-ys@users.noreply.github.com>
2020-01-03 12:02:02 +0900

    add articles#new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,20 @@
 class ArticlesController < ApplicationController
+  before_action :find_project
 
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new()
+  end
+
+
+private
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
 end
